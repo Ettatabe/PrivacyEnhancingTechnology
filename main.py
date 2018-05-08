fefeeade3513a5c0c4dd5fe51219b96924fafa52
Michieldoesburg <M.S.Doesburg@student.tudelft.nl>
2018-05-08 16:54:13 +0200

    moved message construction to function.

--- main.py
+++ main.py
@@ -4,23 +4,27 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 def unsigned(n):
     return n & 0xFFFFFFFF
 
-s = socket.socket()         # Create a socket object
-host = "pets.ewi.utwente.nl" # Get local machine name
-port = 53327                # Reserve a port for your service.
+def create_payload(message):
+    payload = bytearray()
+
+    payload.extend(len(message).to_bytes(4, byteorder='big', signed=False)) # Four bytes to describe the length of message
+    payload.extend(bytes(message, 'ascii'))
+
+    return payload
+
+s = socket.socket()
+host = "pets.ewi.utwente.nl"
+port = 53327
 
 message = 'hey'
-message_length = len(message)
 
-payload = bytearray()
-
-payload.extend(message_length.to_bytes(4, byteorder='big', signed=False))
-payload.extend(bytes(message, 'ascii'))
-
-print(payload)
+payload = create_payload(message)
 
 s.connect((host, port))
 s.send(payload)
+
 print(s.recv(1024))
+
 s.close()                     # Close the socket when done
 
 
