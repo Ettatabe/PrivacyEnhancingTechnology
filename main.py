decbe383e6a5cc22078195d15c27a6bbcf0c4583
Michieldoesburg <M.S.Doesburg@student.tudelft.nl>
2018-05-10 12:22:10 +0200

    fixed sending of message.

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def unsigned(n):
 def create_payload(recipient, message):
     full_string = recipient + ',' + message
     payload = bytearray()
-    payload.extend(len(full_string).to_bytes(4, byteorder='big', signed=False)) # Four bytes to describe the length of message
     payload.extend(bytes(full_string, 'ascii'))
 
     return payload
@@ -29,7 +28,7 @@ def construct_onion_layer(pubkey, iv1, key1, iv2, key2, payload):
 
 s = socket.socket()
 host = "pets.ewi.utwente.nl"
-port = 52872
+port = 52192
 
 '''Generate random IVs and Keys'''
 IV1 = crypto.generate_IV(16)
@@ -48,10 +47,10 @@ E1 = construct_onion_layer(mixnet3_pubkey, IV3, key3, IV1, key1, payload)
 E2 = construct_onion_layer(mixnet2_pubkey, IV2, key2, IV2, key2, E1)
 E3 = construct_onion_layer(mixnet1_pubkey, IV1, key1, IV1, key1, E2)
 
-print(E3)
+final_msg = bytearray(len(E3).to_bytes(4, byteorder='big', signed=False) + E3)
 
 s.connect((host, port))
-s.send(E3)
+s.send(final_msg)
 
 print(s.recv(1024))
 
